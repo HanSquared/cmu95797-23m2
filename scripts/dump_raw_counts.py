3f120ec2321a1735f1c0b91e465a3bc6c6cb1ba8
HanSquared <42755592+HanSquared@users.noreply.github.com>
2023-10-28 16:45:31 -0400

    add scripts

--- scripts/dump_raw_counts.py
+++ scripts/dump_raw_counts.py
@@ -3,3 +3,13 @@ import duckdb
 con = duckdb.connect('main.db')
 tableName = con.sql("SELECT table_name From information_schema.tables;")
 print(tableName)
+#print row count of each table
+# bike_data,central_park_weather,fhv_bases,
+# fhv_tripdata,fhvhv_tripdata,green_tripdata,yellow_tripdata
+print(con.sql("select count(*) from bike_data"))
+print(con.sql("select count(*) from central_park_weather"))
+print(con.sql("select count(*) from fhv_bases"))
+print(con.sql("select count(*) from fhv_tripdata"))
+print(con.sql("select count(*) from fhvhv_tripdata"))
+print(con.sql("select count(*) from green_tripdata"))
+print(con.sql("select count(*) from yellow_tripdata"))
--- sql/dump_raw_schemas.sql
+++ sql/dump_raw_schemas.sql
@@ -3,6 +3,8 @@
 SELECT table_name
 From information_schema.tables;
 --print out schemas
+SELECT table_schema
+FROM information_schema.tables;
 SHOW bike_data;
 SHOW central_park_weather;
 SHOW fhv_bases;
