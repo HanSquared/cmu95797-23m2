7831babb514ff5a438881756b8b8c21c14688743
HanSquared <42755592+HanSquared@users.noreply.github.com>
2023-10-27 22:53:34 -0400

    add dump_raw_schemas.sql

---
+++ sql/dump_raw_schemas.sql
@@ -0,0 +1,12 @@
+--print out table names
+.echo on
+SELECT table_name
+From information_schema.tables;
+--print out schemas
+SHOW bike_data;
+SHOW central_park_weather;
+SHOW fhv_bases;
+SHOW fhv_tripdata;
+SHOW fhvhv_tripdata;
+SHOW green_tripdata;
+SHOW yellow_tripdata;
--- sql/ingest.sql
+++ sql/ingest.sql
@@ -1,21 +1,27 @@
--- ingest taxi, rideshare, bikeshare and weather data
-import csv
+-- ingest taxi, rideshare, bikeshare and weather datas
 create table bike_data as select * from
-read_parquet('./data/taxi/yellow_tripdata*.parquet',
+read_csv_auto('./data/citibike-tripdata.csv.gz',
 union_by_name=True, filename=True); 
 
 create table central_park_weather as select * from 
-read_csv('./data/central_park_weather.csv',
+read_csv_auto('./data/central_park_weather.csv',
 union_by_name=True, filename=True); 
 
 create table fhv_bases as select * from
+read_csv_auto('./data/fhv_bases.csv',
+union_by_name=True, filename=True); 
 
 create table fhv_tripdata as select * from
+read_parquet('./data/taxi/fhv_tripdata.parquet',
+union_by_name=True, filename=True);
 
 create table fhvhv_tripdata as select * from
+read_parquet('./data/taxi/fhvhv_tripdata.parquet',
+union_by_name=True, filename=True);
 
 create table green_tripdata as select * from
-
+read_parquet('./data/taxi/green_tripdata.parquet',
+union_by_name=True, filename=True);
 
 create table yellow_tripdata as select * from 
 read_parquet('./data/taxi/yellow_tripdata*.parquet',
