4b5f2b337a165c905a4de06afe1badd91187d5b0
HanSquared <42755592+HanSquared@users.noreply.github.com>
2023-10-28 17:54:33 -0400

    modified dump_raw_counts.py

--- scripts/dump_raw_counts.py
+++ scripts/dump_raw_counts.py
@@ -6,10 +6,10 @@ print(tableName)
 #print row count of each table
 # bike_data,central_park_weather,fhv_bases,
 # fhv_tripdata,fhvhv_tripdata,green_tripdata,yellow_tripdata
-print(con.sql("select count(*) from bike_data"))
-print(con.sql("select count(*) from central_park_weather"))
-print(con.sql("select count(*) from fhv_bases"))
-print(con.sql("select count(*) from fhv_tripdata"))
-print(con.sql("select count(*) from fhvhv_tripdata"))
-print(con.sql("select count(*) from green_tripdata"))
-print(con.sql("select count(*) from yellow_tripdata"))
+print(con.sql("select count(*) as bike_data_count from bike_data"))
+print(con.sql("select count(*) as central_park_weather_count from central_park_weather"))
+print(con.sql("select count(*) as fhv_bases_count from fhv_bases"))
+print(con.sql("select count(*) as fhv_tripdata_count from fhv_tripdata"))
+print(con.sql("select count(*) as fhvhv_tripdata_count from fhvhv_tripdata"))
+print(con.sql("select count(*) as green_tripdata_count from green_tripdata"))
+print(con.sql("select count(*) as yellow_tripdata_count from yellow_tripdata"))
