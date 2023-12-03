b7764b01c2032373a282dcede74c59ce40a46d93
HanSquared <42755592+HanSquared@users.noreply.github.com>
2023-12-03 12:24:48 -0500

    7 day moving average prcp

--- nyc_transit/analyses/average_time_between_pickups.sql
+++ nyc_transit/analyses/average_time_between_pickups.sql
@@ -1,4 +1,5 @@
--- Find the average time between taxi pick ups per zone
+-- This query finds the average time between taxi pick ups per zone
 -- Use lead or lag to find the next trip per zone for each record
+
 -- then find the time difference between the pick up time for the current record and the next
 -- then use this result to calculate the average time between pick ups per zone.
--- nyc_transit/analyses/seven_day_moving_average_prcp.sql
+++ nyc_transit/analyses/seven_day_moving_average_prcp.sql
@@ -1,2 +1,10 @@
 --This query calculates the 7 day moving average precipitation for every day in the weather data.
 --The 7 day window center on the day in question (for each date, the 3 days before, the date & 3 days after).
+select date,
+    avg(prcp) over(
+        partition by date
+        order by date
+        rows between 3 preceding and 3 following)
+        as seven_day_avg_prcp
+from {{ref('stg__central_park_weather')}} 
+order by date
